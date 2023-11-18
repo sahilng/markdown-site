ed49d476b9e400f7f3c39afc4fb4da9e3621669d
sahilng <sahilng1997@gmail.com>
2023-11-17 19:16:26 -0500

    fix port

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def show_page(page='index'):
     return render_template('page_template.html', content=content)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=8080)
+    app.run(host='0.0.0.0', debug=True, port=80)
